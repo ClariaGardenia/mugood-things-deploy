e873dbc7a48ccdc97b7525fd92109fa8169830d1
Qing <3409434184@qq.com>
2026-06-18 09:39:34 +0800

    docker

---
+++ MuGood-server/muGood-web/src/main/java/com/muGood/web/controller/HealthController.java
@@ -0,0 +1,20 @@
+package com.muGood.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public Map<String, Object> health() {
+        Map<String, Object> result = new HashMap<>();
+        result.put("status", "UP");
+        result.put("service", "muGood-server");
+        result.put("time", System.currentTimeMillis());
+        return result;
+    }
+}
